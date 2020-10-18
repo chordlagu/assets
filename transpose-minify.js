d8e082360b19c986800b5f64aba85ed2b13df70f
chordlagu <68489644+chordlagu@users.noreply.github.com>
2020-10-18 11:31:51 +0700

    Update transpose-minify.js

--- transpose-minify.js
+++ transpose-minify.js
@@ -1,1 +1,275 @@
-!function(d){d.fn.transpose=function(e){function i(e){"m"==e.charAt(e.length-1)&&(e=e.substring(0,e.length-1));for(var a=0;a<p.length;a++)if(e==p[a].name)return p[a]}var m=d.extend({},d.fn.transpose.defaults,e),c=null,p=[{name:"Ab",value:0,type:"F"},{name:"A",value:1,type:"N"},{name:"A#",value:2,type:"S"},{name:"B",value:3,type:"N"},{name:"C",value:4,type:"N"},{name:"C#",value:5,type:"S"},{name:"Db",value:5,type:"F"},{name:"D",value:6,type:"N"},{name:"D#",value:7,type:"S"},{name:"Eb",value:7,type:"F"},{name:"E",value:8,type:"N"},{name:"F",value:9,type:"N"},{name:"F#",value:10,type:"S"},{name:"Gb",value:10,type:"F"},{name:"G",value:11,type:"N"},{name:"G#",value:0,type:"S"}],h=function(e,a,n){var t,s=d(e),u=s.text(),r=1<(t=u).length&&("b"==t.charAt(1)||"#"==t.charAt(1))?t.substr(0,2):t.substr(0,1),l=function(e,a,n){var t=i(e).value+a;11<t?t-=12:t<0&&(t+=12);var s=0;if(0==t||2==t||5==t||7==t||10==t)switch(n.name){case"A":case"A#":case"B":case"C":case"C#":case"D":case"D#":case"E":case"F#":case"G":case"G#":for(;s<p.length;s++)if(p[s].value==t&&"S"==p[s].type)return p[s];default:for(;s<p.length;s++)if(p[s].value==t&&"F"==p[s].type)return p[s]}else for(;s<p.length;s++)if(p[s].value==t)return p[s]}(r,a,n).name+u.substr(r.length);s.text(l);var m,c=s[0].nextSibling;c&&3==c.nodeType&&0<c.nodeValue.length&&"/"!=c.nodeValue.charAt(0)&&(m=o(u.length,l.length,c.nodeValue.length),c.nodeValue=v(" ",m))},o=function(e,a,n){return a<e?n+(e-a):e<a?n-(a-e):n},v=function(e,a){for(var n=[],t=0;t<a;t++)n.push(e);return n.join("")};return d(this).each(function(){var e=d(this).attr("data-key");if(e&&""!=d.trim(e)||(e=m.key),!e||""==d.trim(e))throw"Starting key not defined.";c=i(e);var n=[];d(p).each(function(e,a){c.name==a.name?n.push("<a href='#' class='selected'>"+a.name+"</a>"):n.push("<a href='#'>"+a.name+"</a>")});var l=d(this),a=d("<div class='transpose-keys'></div>");a.html(n.join("")),d("a",a).click(function(e){var a,n,t,s,u,r;return e.preventDefault(),a=l,n=d(this).text(),r=i(n),c.name!=r.name&&(s=c.value,t=(u=r.value)<s?0-(s-u):s<u?u-s+0:0,d("span.c",a).each(function(e,a){h(a,t,r)}),c=r),d(".transpose-keys a").removeClass("selected"),d(this).addClass("selected"),!1}),d(this).before(a);for(var t,s=[],u=d(this).text().split(/\r\n|\n/g),r=0;r<u.length;r++)!function(e){for(var a=e.replace(/\../g," ").replace(/\â€“/g," ").replace(/\-/g," ").replace(/\s+/," ").split(" "),n=0;n<a.length;n++)if(0==!d.trim(a[n]).length&&!a[n].match(m.chordRegex))return!1;return!0}(t=u[r])?s.push("<span>"+t+"</span>"):s.push("<span>"+t.replace(m.chordReplaceRegex,function(e,a,n,t){return a?"<span class='c'>"+a+"</span>":t?"<span class='on'>/</span>":void 0})+"</span>");d(this).html(s.join("\n"))})},d.fn.transpose.defaults={chordRegex:/^[A-G][b\#]?(6\/9|7\-5|7\-9|7\#5|7\#9|7\+5|7\+9|7b5|7b9|7sus2|7sus4|add2|add4|add9|aug|dim|dim7|m\/maj7|m6|m7b5|m7|m9|m11|m13|maj7|maj9|maj11|maj13|mb5|m|sus2|sus4|sus|2|5|6|7|9|11|13)*(\/[A-G][b\#]*)*$/,chordReplaceRegex:/([A-G][b\#]?(6\/9|7\-5|7\-9|7\#5|7\#9|7\+5|7\+9|7b5|7b9|7sus2|7sus4|add2|add4|add9|aug|dim|dim7|m\/maj7|m6|m7b5|m7|m9|m11|m13|maj7|maj9|maj11|maj13|mb5|m|sus2|sus4|sus|2|5|6|7|9|11|13)*)|([/])/g}}(jQuery);
+function renderChords() {
+    $('#song_chords').html("");
+    //var regex = /([A-H][b#]?[m]?[\(]?(2|5|6|7|9|11|13|6\/9|7\-5|7\-9|7 \#5|7\#9|7\+5|7\+9|7b5|7b9|7sus2|7sus4|add2|add4|add9|aug|dim|dim 7|m\|maj7|m6|m7|m7b5|m9|m11|m13|maj|maj7|maj9|maj11|maj13|mb5|m|s us|sus2|sus4){0,2}(\/[A-H])?(\))?)(?=\s|\.|\)|-|\/)/g;
+   
+    var chords = $('pre[data-key] span.c').toArray().map(v => v.textContent); 
+    
+    chords = chords.filter(function(value, index, self) {
+      return self.indexOf(value) === index;
+    });
+    
+    var titleChords = chords
+      .join(' ')
+      .replace(/\+/g, 'p')
+      .replace(/\-/g, 'z')
+      .replace(/\#/g, 'w')
+      .replace(/\//g, 's')
+      .split(' ');
+    
+    var content = '';
+    
+    chords.forEach(function(value, index) {
+    content += '<img class="mobchords" alt="' + value + '" width="41"height="72" src="https://ukula.ru/newchords/' + titleChords[index] + '.png" />';
+      });
+    console.log(content);
+    $('#song_chords').append(content);
+
+}
+
+/*!
+ * iVad song
+ */
+(function($) {
+
+  $.fn.transpose = function(options) {
+    var opts = $.extend({}, $.fn.transpose.defaults, options);
+    
+    var currentKey = null;
+    
+    var keys = [
+      { name: 'Ab',  value: 0,   type: 'F' },
+      { name: 'A',   value: 1,   type: 'N' },
+      { name: 'A#',  value: 2,   type: 'S' },
+      { name: 'Bb',  value: 2,   type: 'F' },
+      { name: 'B',   value: 3,   type: 'N' },
+      { name: 'C',   value: 4,   type: 'N' },
+      { name: 'C#',  value: 5,   type: 'S' },
+      { name: 'Db',  value: 5,   type: 'F' },
+      { name: 'D',   value: 6,   type: 'N' },
+      { name: 'D#',  value: 7,   type: 'S' },
+      { name: 'Eb',  value: 7,   type: 'F' },
+      { name: 'E',   value: 8,   type: 'N' },
+      { name: 'F',   value: 9,   type: 'N' },
+      { name: 'F#',  value: 10,  type: 'S' },
+      { name: 'Gb',  value: 10,  type: 'F' },
+      { name: 'G',   value: 11,  type: 'N' },
+      { name: 'G#',  value: 0,   type: 'S' }
+    ];
+  
+    var getKeyByName = function (name) {
+        if (name.charAt(name.length-1) == "m") {
+          name = name.substring(0, name.length-1);
+        }
+        for (var i = 0; i < keys.length; i++) {
+            if (name == keys[i].name) {
+                return keys[i];
+            }
+        }
+    };
+
+    var getChordRoot = function (input) {
+        if (input.length > 1 && (input.charAt(1) == "b" || input.charAt(1) == "#"))
+            return input.substr(0, 2);
+        else
+            return input.substr(0, 1);
+    };
+
+    var getNewKey = function (oldKey, delta, targetKey) {
+        var keyValue = getKeyByName(oldKey).value + delta;
+
+        if (keyValue > 11) {
+            keyValue -= 12;
+        } else if (keyValue < 0) {
+            keyValue += 12;
+        }
+        
+        var i=0;
+        if (keyValue == 0 || keyValue == 2 || keyValue == 5 || keyValue == 7 || keyValue == 10) {
+            // Return the Flat or Sharp Key
+            switch(targetKey.name) {
+              case "A":
+              case "A#":
+              case "B":
+              case "C":
+              case "C#":
+              case "D":
+              case "D#":
+              case "E":
+              case "F#":
+              case "G":
+              case "G#":
+                  for (;i<keys.length;i++) {
+                    if (keys[i].value == keyValue && keys[i].type == "S") {
+                      return keys[i];
+                    }
+                  }
+              default:
+                  for (;i<keys.length;i++) {
+                    if (keys[i].value == keyValue && keys[i].type == "F") {
+                      return keys[i];
+                    }
+                  }
+            }
+        }
+        else {
+            // Return the Natural Key
+            for (;i<keys.length;i++) {
+              if (keys[i].value == keyValue) {
+                return keys[i];
+              }
+            }
+        }
+    };
+
+    var getChordType = function (key) {
+        switch (key.charAt(key.length - 1)) {
+            case "b":
+                return "F";
+            case "#":
+                return "S";
+            default:
+              return "N";
+        }
+    };
+
+    var getDelta = function (oldIndex, newIndex) {
+        if (oldIndex > newIndex)
+            return 0 - (oldIndex - newIndex);
+        else if (oldIndex < newIndex)
+            return 0 + (newIndex - oldIndex);
+        else
+            return 0;
+    };
+
+    var transposeSong = function (target, key) {
+        var newKey = getKeyByName(key);
+
+        if (currentKey.name == newKey.name) {
+          return;
+        }
+
+        var delta = getDelta(currentKey.value, newKey.value);
+
+        $("span.c", target).each(function (i, el) {
+            transposeChord(el, delta, newKey);
+        });
+        
+        currentKey = newKey;
+    };
+
+    var transposeChord = function (selector, delta, targetKey) {
+        var el = $(selector);
+        var oldChord = el.text();
+        var oldChordRoot = getChordRoot(oldChord);
+        var newChordRoot = getNewKey(oldChordRoot, delta, targetKey);
+        var newChord = newChordRoot.name + oldChord.substr(oldChordRoot.length);
+        el.text(newChord);
+
+        var sib = el[0].nextSibling;
+        if (sib && sib.nodeType == 3 && sib.nodeValue.length > 0 && sib.nodeValue.charAt(0) != "/") {
+            var wsLength = getNewWhiteSpaceLength(oldChord.length, newChord.length, sib.nodeValue.length);
+            sib.nodeValue = makeString(" ", wsLength);
+        }
+    };
+
+    var getNewWhiteSpaceLength = function (a, b, c) {
+        if (a > b)
+            return (c + (a - b));
+        else if (a < b)
+            return (c - (b - a));
+        else
+            return c;
+    };
+
+    var makeString = function (s, repeat) {
+        var o = [];
+        for (var i = 0; i < repeat; i++) o.push(s);
+        return o.join("");
+    }
+    
+    
+    var isChordLine = function (input) {
+        var jem=input.replace(/\../g, " ");
+	var jem2=jem.replace(/\–/g," ");
+	jem2=jem2.replace(/\-/g," ");
+        var tokens = jem2.replace(/\s+/, " ").split(" ");
+        // Try to find tokens that aren't chords
+        // if we find one we know that this line is not a 'chord' line.
+        for (var i = 0; i < tokens.length; i++) {
+            if (!$.trim(tokens[i]).length == 0 && !tokens[i].match(opts.chordRegex))
+                return false;
+        }
+        return true;
+    };
+    
+    var wrapChords = function (input) {
+        return input.replace(opts.chordReplaceRegex,function(str,p1,p2,p3){
+        	if(p1)
+        		return "<span class='c'>" + p1 + "</span>";
+        	else if(p3)
+        		return "<span class='on'>/</span>";
+        });
+    };
+    
+    
+    return $(this).each(function() {
+      var startKey = $(this).attr("data-key");
+     
+      if (!startKey || $.trim(startKey) == "") {
+        startKey = opts.key;
+      }
+
+      if (!startKey || $.trim(startKey) == "") {
+        throw("Starting key not defined.");
+        return this;
+      }
+      
+      currentKey = getKeyByName(startKey);
+
+      // Build tranpose links ===========================================
+      var keyLinks = [];
+      $(keys).each(function(i, key) {
+          if (currentKey.name == key.name)
+              keyLinks.push("<a href='#' class='selected'>" + key.name + "</a>");
+          else
+              keyLinks.push("<a href='#'>" + key.name + "</a>");
+      });
+
+
+      var $this = $(this);
+      var keysHtml = $("<div class='transpose-keys'></div>");
+      keysHtml.html(keyLinks.join(""));
+      $("a", keysHtml).click(function(e) {
+          e.preventDefault();
+          transposeSong($this, $(this).text());
+          $(".transpose-keys a").removeClass("selected");
+          $(this).addClass("selected");
+          return false;
+      });
+      
+      $(this).before(keysHtml);
+
+      var output = [];
+      var lines = $(this).text().split(/\r\n|\n/g);
+      var line, tmp = "";
+
+      for (var i = 0; i < lines.length; i++) {
+          line = lines[i];
+
+          if (isChordLine(line))
+              output.push("<span>" + wrapChords(line) + "</span>");
+          else
+              output.push("<span>" + line + "</span>");
+      };
+
+      $(this).html(output.join("\n"));
+    });
+  };
+
+
+  $.fn.transpose.defaults = {
+    chordRegex: /^[A-G][b\#]?(6\/9|7\-5|7\-9|7\#5|7\#9|7\+5|7\+9|7b5|7b9|7sus2|7sus4|add2|add4|add9|aug|dim|dim7|m\/maj7|m6|m7b5|m7|m9|m11|m13|maj7|maj9|maj11|maj13|mb5|m|sus2|sus4|sus|2|5|6|7|9|11|13)*(\/[A-G][b\#]*)*$/,
+    chordReplaceRegex: /([A-G][b\#]?(6\/9|7\-5|7\-9|7\#5|7\#9|7\+5|7\+9|7b5|7b9|7sus2|7sus4|add2|add4|add9|aug|dim|dim7|m\/maj7|m6|m7b5|m7|m9|m11|m13|maj7|maj9|maj11|maj13|mb5|m|sus2|sus4|sus|2|5|6|7|9|11|13)*)|([/])/g
+  };
+
+})(jQuery);
