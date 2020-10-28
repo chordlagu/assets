ba840b6cc1b387466169d11c48cf16966e43e5d5
chordlagu <68489644+chordlagu@users.noreply.github.com>
2020-10-28 16:07:14 +0700

    Update chord.js

--- chord.js
+++ chord.js
@@ -246,3 +246,5 @@ jQuery("#btnPrint").click(function(){
 jQuery("#instrument-info").tooltip();
 
 jQuery("#btnChordVisibility,#btnPDF,#btnPrint").attr("disabled", false);
+jQuery("pre span:first").css('font-weight', 'bold');
+jQuery("span").filter(function() { return jQuery(this).text().indexOf(" :") >= 0 }).css('font-weight', 'bold', 'color', 'red !important', 'border', 'solid 2px red');
