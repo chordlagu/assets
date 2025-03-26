96eda297045a33c180fd4a4e9d4f300d23276a67
Chordlagu.id <68489644+chordlagu@users.noreply.github.com>
2025-03-26 19:06:56 +0700

    Create transpose_mix_chord.js

---
+++ jscompress/transpose_mix_chord.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+    $(".c").each(function() {
+        let currentSpan = $(this);
+        let nextSpan = currentSpan.next(".on");
+
+        if (nextSpan.length) {
+            let nextChordSpan = nextSpan.next(".c");
+            if (nextChordSpan.length) {
+                let combinedChord = currentSpan.text() + "/" + nextChordSpan.text();
+
+                // Gabungkan ke dalam satu elemen baru
+                let newSpan = $("<span>", {
+                    "class": "c",
+                    "data-chord": combinedChord,
+                    "text": combinedChord
+                });
+
+                // Gantikan elemen lama dengan elemen baru
+                currentSpan.before(newSpan);
+                currentSpan.remove();
+                nextSpan.remove();
+                nextChordSpan.remove();
+            }
+        }
+    });
+};
