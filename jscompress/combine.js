2046ea594e9259bde734d34bda8cda8188c08862
Chordlagu.id <68489644+chordlagu@users.noreply.github.com>
2025-08-19 07:48:16 +0700

    Create combine.js

---
+++ jscompress/combine.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        $(".c").each(function() {
+            let currentSpan = $(this);
+            let nextSpan = currentSpan.next(".on");
+
+            if (nextSpan.length) {
+                let nextChordSpan = nextSpan.next(".c");
+                if (nextChordSpan.length) {
+                    let combinedChord = currentSpan.text() + "/" + nextChordSpan.text();
+
+                    let newSpan = $("<span>", {
+                        "class": "c",
+                        "data-chord": combinedChord,
+                        "text": combinedChord
+                    });
+
+                    // Gantikan elemen lama dengan elemen baru
+                    currentSpan.before(newSpan);
+                    currentSpan.remove();
+                    nextSpan.remove();
+                    nextChordSpan.remove();
+                }
+            }
+        });
+    }, 5000); 
+});
